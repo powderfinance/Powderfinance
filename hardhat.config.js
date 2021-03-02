8c466c7cc6646d6ab413cecd7f9a87f9b9a5d79b
powderfinance <powderfinance@gmail.com>
2021-03-02 16:27:22 +0300

    Added token deployment scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+const INFURA_PROJECT_ID = "";
+const ETHERSCAN_API_ID = "";
+const PRODUCTION_PRIVATE_KEY = "";
+const DEVELOPMENT_PRIVATE_KEY = "";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,6 +23,39 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRODUCTION_PRIVATE_KEY}`]
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEVELOPMENT_PRIVATE_KEY}`]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEVELOPMENT_PRIVATE_KEY}`]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEVELOPMENT_PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEVELOPMENT_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_ID
+  }
 };
 
--- scripts/deploy.mainnet.js
+++ scripts/deploy.mainnet.js
@@ -1,12 +1,19 @@
 const hre = require("hardhat");
 
 async function main() {
-  const {
-    FUND_ADDRESS,
-    ADMIN_ADDRESS,
-    DEVELOPER_ADDRESS,
-    DEVELOPMENT_WALLET
-  } = process.env;
+  const tokensReceiver = "0xD614FCE2c73959EB083B04231f07a079F956B7BB"
+  const Powder = await hre.ethers.getContractFactory("Powder");
 
-  // Add deployment logic and order here ...
+    const powder = await Powder.deploy(tokensReceiver);
+    await powder.deployed();
+    console.log("Powder address:", powder);
 }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
